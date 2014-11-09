2e6662eca35c159cfbf7bd83e15065c3ee330b34
Igor Popov <igor.popov@napa.fi>
2014-11-09 18:04:05 +0200

    Add initial examples
    Signed-off-by: Igor Popov <igor.popov@napa.fi>

---
+++ README.markdown
@@ -0,0 +1,13 @@
+# Jira script examples
+
+The purpose of this project is to serve as documentation for writing scripts that can be run using the free Script Runner add-on for Jira.
+
+## How can I develop the scripts?
+Just run `gradlew` from the command line. This will download all libraries required for creating the scripts and will also generate the IntelliJ IDEA project which you can open by double clicking on the `ipr` file.  
+
+## More information about the Jira API
+Read these resources:  
+- [**Atlassian Jira javadoc**](https://developer.atlassian.com/static/javadoc/jira/reference/packages.html)  
+- [**Atlassian Jira javadoc** (select Jira version)](https://docs.atlassian.com/software/jira/docs/api)  
+- [**Atlassian Jira plugin guide**](https://developer.atlassian.com/display/JIRADEV/JIRA+Plugin+Guide)  
+- [**Performing Issue Operations**](https://developer.atlassian.com/display/JIRADEV/Performing+Issue+Operations)  
---
+++ src/main/groovy/addNewFixVersion.groovy
@@ -0,0 +1,91 @@
+import com.atlassian.jira.component.ComponentAccessor
+import com.atlassian.jira.config.properties.APKeys
+import com.atlassian.jira.event.type.EventDispatchOption
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.issue.UpdateIssueRequest
+import com.atlassian.jira.issue.label.LabelManager
+import com.atlassian.jira.issue.search.SearchProvider
+import com.atlassian.jira.jql.parser.JqlQueryParser
+import com.atlassian.jira.web.bean.PagerFilter
+
+finalMessage = ""
+
+def mainMethod() {
+    // This script must be run from Jira -> Administration -> Add-ons -> Script Console
+    jqlQuery = "project = DEMO and text ~ 'shortcut'"
+    logImportantMessage "Executing query: <pre>${jqlQuery}</pre>"
+    def issues = findIssues(jqlQuery)
+
+    logImportantMessage "Found ${issues.size()} issues. Here they are:"
+    logIssues(issues)
+
+    def newLabel = "productivity"
+    logImportantMessage "Will add the ${newLabel} to all mentioned issues..."
+    issues.each { issue -> addLabelToIssue(issue, newLabel) }
+
+    // updateIssues(issues)
+    logImportantMessage "Successfully updated all the mentioned issues..."
+
+    return finalMessage // the returned value is shown after the execution in Jira's Web Script Console
+}
+
+def logIssues(Collection<MutableIssue> issues) {
+    logMessage "<pre>"
+    issues.each { issue -> logMessage formatIssue(issue) }
+    logMessage "</pre>"
+}
+
+def logMessage(Object message) {
+    finalMessage += "${message}<br/>"
+}
+
+def logImportantMessage(Object message) {
+    logMessage "<strong>${message}</strong>"
+}
+
+def formatIssue(MutableIssue issue) {
+    def issueLink = getIssueLink(issue)
+    def htmlLink = "<a href=\"${issueLink}\">${issue.key}</a>"
+    "<strong>${htmlLink}</strong> - ${issue.summary}"
+}
+
+def getIssueLink(MutableIssue issue) {
+    def properties = ComponentAccessor.applicationProperties
+    def jiraBaseUrl = properties.getString(APKeys.JIRA_BASEURL)
+    "${jiraBaseUrl}/browse/${issue.key}"
+}
+
+def findIssues(String jqlQuery) {
+    def issueManager = ComponentAccessor.issueManager
+    def user = ComponentAccessor.jiraAuthenticationContext.user
+    def jqlQueryParser = ComponentAccessor.getComponent(JqlQueryParser.class)
+    def searchProvider = ComponentAccessor.getComponent(SearchProvider.class)
+
+    def query = jqlQueryParser.parseQuery(jqlQuery)
+    def results = searchProvider.search(query, user, PagerFilter.unlimitedFilter)
+    results.issues.collect { issue -> issueManager.getIssueObject(issue.id) }
+}
+
+def updateIssues(Collection<MutableIssue> issues) {
+    def user = ComponentAccessor.jiraAuthenticationContext.user
+    def issueManager = ComponentAccessor.issueManager
+    issues.each { issue ->
+        issueManager.updateIssue(user, issue, createIssueUpdateRequest())
+    }
+}
+
+def createIssueUpdateRequest() {
+    new UpdateIssueRequest.UpdateIssueRequestBuilder()
+            .eventDispatchOption(EventDispatchOption.DO_NOT_DISPATCH)
+            .sendMail(false)
+            .build()
+}
+
+def addLabelToIssue(MutableIssue issue, String label) {
+    def labelManager = ComponentAccessor.getComponent(LabelManager.class)
+    def user = ComponentAccessor.jiraAuthenticationContext.user.directoryUser
+    def sendEmailUpdates = false
+    labelManager.addLabel(user, issue.id, label, sendEmailUpdates)
+}
+
+mainMethod()
